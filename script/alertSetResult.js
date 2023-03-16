59667672e4dbc81e6fd4049d53474a81b7e6d4e1
haingo96 <ngohoanghai96@gmail.com>
2023-03-16 13:39:09 +0700

    remove unused code from alertSetResult

--- script/alertSetResult.js
+++ script/alertSetResult.js
@@ -1,9 +1,7 @@
 function alertSetResult(gameBoardElement) {
     if (gameBoardElement === 'X') {
         alert('X won!')
-        return gameBoardElement
     } else {
         alert('O won!')
-        return gameBoardElement
     }
 }

dbe050755bfe10de9c69d7f544bfe713bdee7a86
haingo96 <ngohoanghai96@gmail.com>
2023-03-17 11:05:29 +0700

    fix checked cell changable

--- script/inputValueAndCheck.js
+++ script/inputValueAndCheck.js
@@ -1,18 +1,20 @@
 function inputValueAndCheck(row, col) {
-    if (turn % 2 !== 0) {
-        gameBoard[row][col] = 'X'
-        document.getElementById(`gameBoard[${row}][${col}]`).innerText = 'X'
-        document.getElementById(`gameBoard[${row}][${col}]`).style.color = 'red'
-    } else {
-        gameBoard[row][col] = 'O'
-        document.getElementById(`gameBoard[${row}][${col}]`).innerText = 'O'
-        document.getElementById(`gameBoard[${row}][${col}]`).style.color = 'blue'
-    }
+    if (gameBoard[row][col] !== 'X' && gameBoard[row][col] !== 'O') {
+        if (turn % 2 !== 0) {
+            gameBoard[row][col] = 'X'
+            document.getElementById(`gameBoard[${row}][${col}]`).innerText = 'X'
+            document.getElementById(`gameBoard[${row}][${col}]`).style.color = 'red'
+        } else {
+            gameBoard[row][col] = 'O'
+            document.getElementById(`gameBoard[${row}][${col}]`).innerText = 'O'
+            document.getElementById(`gameBoard[${row}][${col}]`).style.color = 'blue'
+        }
 
-    requestAnimationFrame(() => {
-        setTimeout(() => {
-            checkGameBoard(gameBoard)
-            turn++
+        requestAnimationFrame(() => {
+            setTimeout(() => {
+                checkGameBoard(gameBoard)
+                turn++
+            })
         })
-    })
+    }
 }
